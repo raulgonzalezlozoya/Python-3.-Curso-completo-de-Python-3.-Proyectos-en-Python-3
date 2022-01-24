0e21d966edffbc1e45ef5bb84e2a33efbb12fb53
raulgonzalezlozoya <98315234+raulgonzalezlozoya@users.noreply.github.com>
2022-01-24 15:11:50 +0100

    Ejercicio 2
    Ejercicio 2

---
+++ Variables.py
@@ -0,0 +1,19 @@
+# Variables
+numero = 5
+print (numero)
+
+cadena = "Hola mundo"
+print (cadena)
+
+Numero = 5
+numero = 6
+print (Numero)
+print (numero)
+
+cadena1 = "Hola "
+cadena2 = "mundo"
+cadenanueva = cadena1 + cadena2
+print (cadenanueva)
+
+print (type (numero))
+
