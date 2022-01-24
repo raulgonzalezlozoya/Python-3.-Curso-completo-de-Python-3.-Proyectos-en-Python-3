67ce5ed38fc3ba440670edab427a86c8bb37159a
raulgonzalezlozoya <98315234+raulgonzalezlozoya@users.noreply.github.com>
2022-01-24 15:29:06 +0100

    Ejercicio 4
    Ejercicio 4

---
+++ Conversiones de tipos de datos.py
@@ -0,0 +1,29 @@
+# Conversiones de tipos de datos
+
+numero = 5
+cadena = str(numero)
+print (cadena)
+
+print (numero)
+
+print (type(cadena))
+
+cadena = '25'
+numero = int(cadena)
+print (numero)
+
+numero2 = 4
+suma = numero + numero2
+print (suma)
+
+cadena = '25.7'
+numerodecimal = float(cadena)
+print (numerodecimal)
+
+numerodecimal2 = 2.3
+sumadecimal = numerodecimal+numerodecimal2
+print (sumadecimal)
+
+print(type(sumadecimal))
+
+
