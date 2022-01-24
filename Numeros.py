556039e8646e4dec9105595b598642db5fff86b2
raulgonzalezlozoya <98315234+raulgonzalezlozoya@users.noreply.github.com>
2022-01-24 15:18:52 +0100

    Ejercicio 3
    Ejercicio 3

---
+++ Numeros.py
@@ -0,0 +1,23 @@
+# Números
+
+numero1 = 5
+print (numero1)
+
+print (type(numero1))
+
+numerodecimal = 3.7
+print (numerodecimal)
+
+print (type(numerodecimal))
+
+numero1 = 5
+numero2 = 3
+suma = numero1 + numero2
+print (suma)
+print(type(suma))
+
+suma = numero1 + numerodecimal
+print (suma)
+print(type(suma))
+
+
