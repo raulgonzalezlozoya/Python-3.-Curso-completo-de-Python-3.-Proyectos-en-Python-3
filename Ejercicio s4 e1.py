2cd3e2e1b885c432f4bc21e55ec91fa8700609ea
raulgonzalezlozoya <98315234+raulgonzalezlozoya@users.noreply.github.com>
2022-01-24 20:38:10 +0100

    Sección 4 ejercicio 1
    Sección 4 ejercicio 1

---
+++ Ejercicio s4 e1.py
@@ -0,0 +1,22 @@
+# Ejercicio
+
+# Crear una variable "numero1" que tenga el valor 5. Indicar de que tipo es la variable "numero1".
+
+numero1 = 5
+print (type(numero1))
+
+# Crear otra variable "numero2" que tenga el valor 6.5. Indicar de que tipo es la variable "numero2".
+
+numero2 = 6.5
+print (type(numero2))
+
+# Crear otra variable "numero3" que tenga el valor 7. Indicar de que tipo es la variable "numero3".
+
+numero3 = 7
+print(type(numero3))
+
+#Crear otra variable "suma" que tenga el valor de la suma de los 3 números anteriores (numero1, numero2, numero3). Mostrar el resultado de la variable "suma". Indicar de que tipo es la variable "suma".
+
+suma = numero1+numero2+numero3
+print (suma)
+print (type(suma))
