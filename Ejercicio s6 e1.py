0598e0811afe967f9f485b887c5f95cab89adf02
raulgonzalezlozoya <98315234+raulgonzalezlozoya@users.noreply.github.com>
2022-01-25 13:34:35 +0100

    Sección 6 ejercicio 1
    Sección 6 ejercicio 1

---
+++ Ejercicio s6 e1.py
@@ -0,0 +1,7 @@
+# Ejercicio
+
+# Crear una variable "cadena" que contiene el texto "Esto es un texto de ejemplo". Según la posición de cada letra en la cadena, calcular que valores (x,y) hay que poner para seleccionar solo la palabra "texto". cadena[x:y] ="texto".
+
+cadena = "Esto es un texto de ejemplo"
+print(cadena)
+print(cadena[11:16])
