092e6a7143d0cabe320cc8bfbc316192aa55cc16
raulgonzalezlozoya <98315234+raulgonzalezlozoya@users.noreply.github.com>
2022-01-25 14:09:14 +0100

    Sección 6 ejercicio 4
    Sección 6 ejercicio 4

---
+++ Ejercicio s6 e4.py
@@ -0,0 +1,41 @@
+# Ejercicio
+
+# 1. Imprime por pantalla el texto "Introduce el primer número".
+
+print("Introduce el primer número")
+
+# 2. Crear la variable "dato1" con el primer valor introducido en el paso anterior.
+
+dato1 = input()
+
+# 3. Imprime por pantalla el texto "Introduce el segundo número".
+
+print("Introduce el segundo número")
+
+# 4. Crear la variable "dato2" con el primer valor untroducido en el paso anterior.
+
+dato2 = input()
+
+# 5. Convertir la variable "dato1" en una variable numérica denominada "numero1".
+
+numero1 = int(dato1)
+
+# 6. Convertir la variable "dato2" en una variable numérica denominada "numero2".
+
+numero2 = int(dato2)
+
+# 7. Crear la variable "suma" con la suma de "numero1" y "numero2".
+
+suma = numero1 + numero2
+
+#8. Convertir la variable "suma" en una variable de texto denominada "strSuma".
+
+strSuma = str(suma)
+
+#9. Crear la variable "resultado" con la concatenación de "La suma es " y "strSuma".
+
+resultado = "La suma es " + strSuma
+
+#10. Imprimir el valor "resultado".
+
+print(resultado)
