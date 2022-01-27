346378407868c46ba31569ae0ed1cbbc5a1c68a1
raulgonzalezlozoya <98315234+raulgonzalezlozoya@users.noreply.github.com>
2022-01-27 10:33:22 +0100

    Sección 8 ejercicio 1
    Sección 8 ejercicio 1

---
+++ Ejercicio_s8_e1.py
@@ -0,0 +1,24 @@
+# Ejercicio
+
+# Crear una variable "nota1" que tenga valor 6.
+
+nota1 = 6
+
+# Crear otra variable "nota2" que tenga el valor 4.
+
+nota2 = 4
+
+# Crear otra variable "nota3" que tenga el valor 7.
+
+nota3 = 7
+
+# Crear otra variable "nota_media" que tenga el valor medio de las 3 notas anteriores.
+
+nota_media = (nota1 + nota2 + nota3) / 3
+print(nota_media)
+
+# Crear otra variable "nota_final" que tenga el valor "apobado" (mayor o igual a 5).
+
+if (nota_media >= 5):
+	nota_final = "aprobado"
+	print(nota_final)
