94235c2c17da25324c6e6acbe40afd33241f17ec
raulgonzalezlozoya <98315234+raulgonzalezlozoya@users.noreply.github.com>
2022-01-28 13:24:53 +0100

    Sección 10 ejercicio 3
    Sección 10 ejercicio 3

---
+++ Ejercicio_s10_e3.py
@@ -0,0 +1,32 @@
+# Ejercicio
+
+# 1. Crear un variable "conjunto" que sea un conjunto de los valores 1,2,3,4 y 5.
+
+conjunto ={1,2,3,4,5}
+
+# 2. Mostrar el valor de la variable "conjunto".
+
+print(conjuno)
+
+# 3. Añadir los números 6,7,8 y 9 a la variable "conjunto".
+
+conjunto.add(6)
+conjunto.add(7)
+conjunto.add(8)
+conjunto.add(9)
+
+# 4. Mostrar ahora el valor de la variable "conjunto".
+
+print(conjuno)
+
+# 5. Eliminar el valor 9 de la variable "conjunto".
+
+conjunto.remove(9)
+
+# 6. Mostar ahora el valor de la variable "conjunto".
+
+print(conjuno)
+
+# 7. Verificar que tipo de dato es la variable "conjunto" mediante type().
+
+print(type(conjunto))
